b5de48fc4e53abf747c483e93b51bac9ab724c91
Rajae Gregory <rajgreg306@gmail.com>
2025-11-30 14:00:27 -0500

    Addedd html table

--- world.php
+++ world.php
@@ -15,8 +15,22 @@ $list = $newstmt -> fetchAll(PDO::FETCH_ASSOC);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+<table class="country-table">
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+
+  </tr>
 <?php foreach ($list as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  
+  <tr>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['continent'];?></td>
+    <td><?= $row['independence_year'];?></td>
+    <td><?= $row['head_of_state'];?></td>
+
+  </tr>
 <?php endforeach; ?>
-</ul>
+</table>
