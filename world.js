fbc1f75603bd5fe7fdad1100be873a470709c7fd
Rajae Gregory <rajgreg306@gmail.com>
2025-11-30 15:32:30 -0500

    Final Functionality

--- world.js
+++ world.js
@@ -1,23 +1,37 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("lookup");
+    const country_button = document.getElementById("lookup");
+    const city_button = document.getElementById("cities");
 
-    button.addEventListener("click", () => {
-        const httpRequest = new XMLHttpRequest();
+    country_button.addEventListener("click", () => {
+        
         let country = document.getElementById("country").value;
+        fetchData(country, "")
+    });
+
+    city_button.addEventListener("click", ()=> {
+        let country = document.getElementById("country").value;
+        fetchData(country, "cities")
+    })
+    
+    
+    const fetchData =(country, lookup) => {
+        const httpRequest = new XMLHttpRequest();
         let url = `http://localhost/info2180-lab5/info2180-lab5/world.php?country=${country}`
+        if(lookup){
+            url += `&lookup=${lookup}`;
+        }
         httpRequest.open('GET', url)
 
-    httpRequest.onload = function() {
-        if (httpRequest.status === 200){
-            document.getElementById('result').innerHTML = httpRequest.responseText;
-        } else{
-            document.getElementById('result').innerHTML = "Error fetching countries";
+        httpRequest.onload = function() {
+            if (httpRequest.status === 200){
+                document.getElementById('result').innerHTML = httpRequest.responseText;
+            } else{
+                document.getElementById('result').innerHTML = "Error fetching countries";
+            }
         }
-    }
     
-    httpRequest.send();
-
-    })
+        httpRequest.send();
+    }
 })
 
--- world.php
+++ world.php
@@ -3,34 +3,56 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$country = $_GET['country'];
+$country = isset($_GET['country']) ? $_GET['country'] : '';
+$lookup  = isset($_GET['lookup']) ? $_GET['lookup'] : '';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$newstmt = $conn ->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+#$stmt = $conn->query("SELECT * FROM countries");
 
-$list = $newstmt -> fetchAll(PDO::FETCH_ASSOC);
+#$newstmt = $conn ->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+#$citystmt = $conn ->query("SELECT * FROM cities WHERE name LIKE '%$country%'");
 
-?>
-<table class="country-table">
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
+#$list = $newstmt -> fetchAll(PDO::FETCH_ASSOC);
 
-  </tr>
-<?php foreach ($list as $row): ?>
-  
-  <tr>
-    <td><?= $row['name']; ?></td>
-    <td><?= $row['continent'];?></td>
-    <td><?= $row['independence_year'];?></td>
-    <td><?= $row['head_of_state'];?></td>
+#$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  if($lookup === "cities"){
+    $stmt = $conn->query(
+      "SELECT cities.name AS city_name, cities.district, cities.population
+      FROM cities
+      JOIN countries ON country_code = countries.code
+      WHERE countries.name LIKE '%$country%'
+      ");
+      
+      $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  </tr>
-<?php endforeach; ?>
-</table>
+      echo "<table class='country-table'>
+            <tr><th>Name</th><th>District</th><th>Population</th></tr>";
+    foreach ($list as $row) {
+        echo "<tr>
+                <td>{$row['city_name']}</td>
+                <td>{$row['district']}</td>
+                <td>{$row['population']}</td>
+              </tr>";
+    }
+    echo "</table>";
+
+      
+  } else{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table class='country-table'>
+            <tr><th>Name</th><th>Continent</th><th>Independence</th><th>Head of State</th></tr>";
+    foreach ($list as $row) {
+        echo "<tr>
+                <td>{$row['name']}</td>
+                <td>{$row['continent']}</td>
+                <td>{$row['independence_year']}</td>
+                <td>{$row['head_of_state']}</td>
+              </tr>";
+    }
+    echo "</table>";
+}
+    
